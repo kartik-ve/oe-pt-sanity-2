deb2dfd205a8e3c7f96e9e1c224a6bbe2a066e6c
Kartik Verma <kartikve@amdocs.com>
2026-03-03 16:41:27 +0530

    Minor changes

--- scripts/prepare_reports.bat
+++ scripts/prepare_reports.bat
@@ -1,9 +1,10 @@
 set BUILD_DIR=%BUILD_NUMBER%
 set ERROR_DIR=%BUILD_DIR%\error_logs
 set REMOTE_BUILD=/users/gen/omswrk1/JEE/OMS/logs/OmsDomain/OmsServer/sanity_logs/%JOB_NAME%_%BUILD_NUMBER%
+set HOST=illnqw%ENV%
 
 if not exist "%ERROR_DIR%" mkdir "%ERROR_DIR%"
-scp omswrk1@illnqw%ENV%:%REMOTE_BUILD%/*.err ^
+scp omswrk1@%HOST%:%REMOTE_BUILD%/*.err ^
   "%ERROR_DIR%"
 
 java -cp "java\local\target\classes;java\local\target\dependency\*" ^
